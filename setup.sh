f3dd9fb5030b3b79cafa96cf1c6feddda51e5354
Zaron <zaron.thompson@gmail.com>
2024-02-29 17:22:45 -0800

    setup testing

--- setup.sh
+++ setup.sh
@@ -1,12 +1,48 @@
 #!/usr/bin/env bash
 
-#important file paths
+# Install, Uninstall, and Update script for adding Parental Controls to RetroPie
+
+# call 'setup.sh' to autmaticly install or update an existing installation
+# call 'setup.sh uninstall' to remove
+
+
+# file paths
 RUNCOMMAND="/opt/retropie/supplementary/runcommand/runcommand.sh"
 AUTOSTART="/opt/retropie/configs/all/autostart.sh"
 ONEND="/opt/retropie/configs/all/runcommand-onend.sh"
+INSTALLDIR="$HOME/RetroPie-Parental"
 
+# script inserts
+AUTOSTART_LINE="$INSTALLDIR/web.py prod &"
+RUN_LINE1="source \"$INSTALLDIR/runcommand-parental.sh\""
+RUN_LINE2="    parental-control"
 
 #FUNCTIONS
+
+setup_colors() {
+  # check if stdout is a terminal...
+  if test -t 1; then
+
+    # see if it supports colors...
+    ncolors=$(tput colors)
+
+    if test -n "$ncolors" && test $ncolors -ge 8; then
+        bold="$(tput bold)"
+        underline="$(tput smul)"
+        standout="$(tput smso)"
+        normal="$(tput sgr0)"
+        black="$(tput setaf 0)"
+        red="$(tput setaf 1)"
+        green="$(tput setaf 2)"
+        yellow="$(tput setaf 3)"
+        blue="$(tput setaf 4)"
+        magenta="$(tput setaf 5)"
+        cyan="$(tput setaf 6)"
+        white="$(tput setaf 7)"
+        gray=$(tput setaf 8)
+    fi
+  fi
+}
 
 # params: package name
 insure_package() {
@@ -15,7 +51,7 @@ insure_package() {
   local exitCode=$?
   if [ $exitCode != 0 ]
   then
-    echo installing missing package: $package
+    echo ${magenta}installing missing package: $package ${normal}
     sudo apt install "$package" -y
     local exitCode=$?
     # try updating package list - one time only
@@ -29,17 +65,20 @@ insure_package() {
   fi
   if [ $exitCode != 0 ]
   then
-    echo "the required package: '$package' could not be installed.  please install the package manualy and try this script again." >&2
+    echo "${red}The required package: '$package' could not be installed.  Please install the package manually and try this script again.${normal}" >&2
     exit 1
   fi
 }
 
 insure_os() {
   local sysName=$(uname -n)
-  if [ "$sysName" != "reropie" ]
+  if [ "$sysName" != "retropie" ]
   then
-    echo "This setup script was intended for the RetroPie os installed from an image for a raspberry pi.  This system apears to be diffrent."
-    read -r -p "Do you want to continue? [Y/n] " response
+    echo "${yellow}This setup script was intended for the RetroPie os installed from an image for a raspberry pi.  
+    As such it was expecting the host name to be 'retropie'.  
+    Instead, your host name is '$(uname -n)'.  
+    This setup was tested for retropie os and may have errors installing on a diffrent setup.${normal}"
+    read -r -p "${bold}Do you want to continue? [Y/n] ${normal}" response
     if [[ "$response" =~ ^([nN][oO]|[nN])$ ]]
     then
         exit 1
@@ -51,8 +90,8 @@ insure_file() {
   local file="$1"
   if [ ! -e "$file" ]
   then
-    echo "'$file' is missing."
-    echo "RetroPie does not seem to be installed on this system" >&2
+    echo "'$file' is missing but was exprected for RetroPie"
+    echo "${red}RetroPie does not seem to be installed on this system${normal}" >&2
     exit 1
   fi
 }
@@ -83,7 +122,7 @@ remove_line() {
 }
 
 install() {
-  echo "This will setup Parental Controls on your RetroPie"
+  echo "${standout}setting up Parental Controls on your RetroPie...${normal}"
 
   # is this the right os
   insure_os
@@ -101,19 +140,22 @@ install() {
 
   # download the app
   cd ~
-  if [ ! -e "RetroPie-Parental" ]
+  if [ ! -e "$INSTALLDIR" ]
   then
+    echo "${magenta}Downloading RetroPie Parental software...${normal}"
     git clone https://github.com/zaront/RetroPie-Parental.git
   fi
-  cd "RetroPie-Parental"
-  installDir=$(pwd)
+  cd "$INSTALLDIR"
 
   # install python packages
+  echo "${magenta}Downloading python requirements...${normal}"
   pip3 install -r requirements.txt
 
   # ==integrate with RetroPie sys by doing the following:
+  echo "${blue}Integrating Parental Controls into RetroPie...${normal}"
 
   # --add hard link to runcommand-onend.sh file
+  echo "${magenta}Adding runcommand-onend.sh...${normal}"
   if [ -f "$ONEND" ]
   then
     rm "$ONEND"
@@ -121,29 +163,32 @@ install() {
   ln runcommand-onend.sh $ONEND
 
   # --add website to autostart along with RetroPie
-  local autostart_line="$installDir/web.py prod &"
-  get_line "$autostart_line" $AUTOSTART
+  echo "${magenta}Adding website to autostart along with RetroPie...${normal}"
+  get_line "$AUTOSTART_LINE" $AUTOSTART
   if [ "$get_line_result" == 0 ]
   then
-    insert_line "$autostart_line" $AUTOSTART 0
+    insert_line "$AUTOSTART_LINE" $AUTOSTART 1
+  else
+    echo "${gray}skipped: changes have been already made${normal}"
   fi
 
   # --add parental support to runcommand.sh
+  echo "${magenta}Adding parental support to runcommand.sh...${normal}"
   local run_find1="source \"\$ROOTDIR/lib/inifuncs.sh\""
-  local run_line1="source \"$installDir/runcommand-parental.sh\""
-  get_line "$run_line1" $RUNCOMMAND
+  get_line "$RUN_LINE1" $RUNCOMMAND
   if [ "$get_line_result" == 0 ]
   then
     get_line "$run_find1" $RUNCOMMAND
     if [ "$get_line_result" != 0 ]
     then
-      insert_line "$run_line1" $RUNCOMMAND $(($get_line_result + 1))
+      insert_line "$RUN_LINE1" $RUNCOMMAND $(($get_line_result + 1))
     fi
+  else
+    echo "${gray}skipped: 1st change has already been already made${normal}"
   fi
   local run_find2="function runcommand() {"
   local run_find3="rm -f \"\$LOG\""
-  local run_line2="parental-control"
-  get_line "$run_line2" $RUNCOMMAND
+  get_line "$RUN_LINE2" $RUNCOMMAND
   if [ "$get_line_result" == 0 ]
   then
     get_line "$run_find2" $RUNCOMMAND
@@ -152,41 +197,118 @@ install() {
       get_line "$run_find3" $RUNCOMMAND $get_line_result
       if [ "$get_line_result" != 0 ]
       then
-        insert_line "$run_line2" $RUNCOMMAND $get_line_result
+        insert_line "$RUN_LINE2" $RUNCOMMAND $get_line_result
       fi
     fi
+  else
+    echo "${gray}skipped: 2nd change has already been already made${normal}"
   fi
 
-  echo "Parental Controls are installed at ~/RetroPie_Parental"
-  echo "Please Reboot your RetroPie to complete your installation"
-  echo
-  echo "the parental website is at:"
-  echo "http://retropie.local:8080" or http://$(hostname -I | cut -f1 -d' '):8080 
-  echo "username: admin"
-  echo "password: retropie"
+  echo "${green}SUCCESS: Parental Controls are installed at ~/RetroPie_Parental${normal}
+  ${bold}Please Reboot your RetroPie to complete your installation${normal}
+
+  The parental website is at:
+    ${blue}http://retropie.local:8080${normal} or ${blue}http://$(hostname -I | cut -f1 -d' '):8080 ${normal}
+    username: ${magenta}admin${normal}
+    password: ${magenta}retropie${normal}"
 }
 
 update() {
-  echo "updating to the latest version"
-  if [ -e "~/RetroPie-Parental/.git/"]
+  echo "${standout}Updating to the latest version${normal}"
+
+  # validate repository
+  if [ ! -e "$INSTALLDIR/.git/" ]
   then
-    cd "~/RetroPie-Parental"
-    
+    echo "${red}Installation can't be updated because it wasn't found to be setup using git.${normal}
+    ${gray}To have updates work, you will need to uninstall and reinstall
+    by running 'setup.sh uninstall' then 'setup.sh install' to fix this${normal}"
+    exit 1
   fi
+
+  # update repository
+  cd "$INSTALLDIR"
+  local prevCommit=$(git rev-parse HEAD)
+  git pull
+  local ret=$?
+  if [ $ret != 0 ]
+  then
+    # if it failed - revert changes and try again
+    if [[ $(git status --porcelain --untracked-files=no) ]]; then
+      git reset --hard
+      git pull
+      ret=$?
+    fi
+  fi
+
+  # run install if there was an update
+  if [ $ret == 0 ]
+  then
+    local currentCommit=$(git rev-parse HEAD)
+    if [ "$prevCommit" != "$currentCommit" ]
+    then
+      echo "${blue}Updated found and downloaded.${normal}"
+      echo "${gray}Installing...${normal}"
+      ./setup.sh install
+    else
+      echo "${blue}No new version found.${normal}"
+    fi
+  else
+    echo "${red}Your git repository could not be updated.  Update it manually and try this script again${normal}"
+    exit 1
+  fi
+
+  echo "${green}Your Parental Controls are at the latest version.${normal}"
 }
 
 uninstall() {
-  echo uninstall
+  echo "${standout}Uninstalling Parental Control for RetroPie...${normal}"
+
+  # does the folder exist
+  echo "$INSTALLDIR"
+  if [ ! -e "$INSTALLDIR" ]
+  then
+    echo "${red}Parental Controls not found, or already uninstalled${normal}"
+    exit 1
+  fi
+
+  # remove website startup
+  get_line "$AUTOSTART_LINE" $AUTOSTART
+  if [ "$get_line_result" != 0 ]
+  then
+    remove_line $AUTOSTART $get_line_result
+  fi
+
+  # remove runcommand-onend.sh file
+  rm $ONEND
+
+  # remove runcommand support for Parental Control
+  local run_find1="source \"\$ROOTDIR/lib/inifuncs.sh\""
+  get_line "$RUN_LINE1" $RUNCOMMAND
+  if [ "$get_line_result" != 0 ]
+  then
+    remove_line $RUNCOMMAND $get_line_result
+  fi
+  get_line "$RUN_LINE2" $RUNCOMMAND
+  if [ "$get_line_result" != 0 ]
+  then
+    remove_line $RUNCOMMAND $get_line_result
+  fi
+
+  # remove folder
+  rm -rf "$INSTALLDIR"
+
+  echo "${blue}Parental Controls are have been uninstalled${normal}"
+  echo "${bold}Please Reboot your RetroPie to complete its removal${normal}"
 }
 
 auto_choose_action() {
-  echo "Checking if already installed"
+  echo "${gray}Verifying existing installation...${normal}"
   local installed=0
-  if [ -f "~/RetroPie-Parental/runcommand-onend.sh" ] && [ -f "$ONEND" ]
+  if [ -f "$INSTALLDIR/runcommand-onend.sh" ] && [ -f "$ONEND" ]
   then
-    local hardlink1=$(ls -i "~/RetroPie-Parental/runcommand-onend.sh" | cut -f1 -d" ")
+    local hardlink1=$(ls -i "$INSTALLDIR/runcommand-onend.sh" | cut -f1 -d" ")
     local hardlink2=$(ls -i "$ONEND" | cut -f1 -d" ")
-    [ $hardlink1 == $hardlink2 ] && $installed=1
+    [ $hardlink1 == $hardlink2 ] && installed=1
   fi
 
   # if not installed then install
@@ -203,26 +325,26 @@ auto_choose_action() {
   
   # message about uninstalling
   echo "
-
-update by running '~/RetroPie-Parental/setup.sh update'
-uninstall by running '~/RetroPie-Parental/setup.sh uninstall'
+  ${gray}
+  In the future you can update to the latest release by running '${magenta}~/RetroPie-Parental/setup.sh update${gray}'
+  or remove Parental Controls by running '${normal}${magenta}~/RetroPie-Parental/setup.sh uninstall${gray}'${normal}
 "
 }
 
 show_help() {
   echo "
-Setup script for adding Parental Controls to RetroPie
+${blue}Setup script for adding Parental Controls to RetroPie${normal}
 
 commands:
-  install          - download and install Parental Controls
-  update           - update to the lattest version
-  uninstall        - remove Parental Controls
+  ${magenta}install${normal}          ${gray}- download and install Parental Controls${normal}
+  ${magenta}update${normal}           ${gray}- update to the lattest version${normal}
+  ${magenta}uninstall${normal}        ${gray}- remove Parental Controls${normal}
 "
 }
 
 #COMMANDS
 
-
+setup_colors
 case "$1" in
 
   "install")
